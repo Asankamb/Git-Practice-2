9d1b99f81f2d1fbca3d476e2265b11913cd3c319
Asankamb <47291721+Asankamb@users.noreply.github.com>
2023-03-12 15:34:18 +0530

    test added

--- src/test/java/net/phptravels/Login.java
+++ src/test/java/net/phptravels/Login.java
@@ -5,5 +5,6 @@ public class Login {
     public void setText(){
         System.out.println("Hello Sunny");
         System.out.println("hello mickey");
+        System.out.println("Test");
     }
 }
