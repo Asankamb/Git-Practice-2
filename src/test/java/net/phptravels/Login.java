5054adf97be288d4ccdcfd5caade12c4c1aa9230
Asankamb <47291721+Asankamb@users.noreply.github.com>
2023-03-12 15:28:18 +0530

    Update Login.java

--- src/test/java/net/phptravels/Login.java
+++ src/test/java/net/phptravels/Login.java
@@ -3,7 +3,7 @@ package net.phptravels;
 public class Login {
 
     public void setText(){
-        System.out.println("Hello Cunny");
+        System.out.println("Hello Sunny");
         System.out.println("hello mickey");
     }
 }
